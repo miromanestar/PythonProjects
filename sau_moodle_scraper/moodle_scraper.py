a673072ee1a5ff54705c8d9e2357343718b2f3ab
Miro Manestar <miroimanestar@gmail.com>
2021-01-21 12:49:32 -0500

    Began writing parser for quizez

--- sau_moodle_scraper/moodle_scraper.py
+++ sau_moodle_scraper/moodle_scraper.py
@@ -8,6 +8,12 @@ file = codecs.open('raw.html', 'r') #Get HTML file text
 page = BeautifulSoup(file, 'lxml')
 name = page.title.text
 
-questions = page.find_all('div', class_='que').find('p')
+qContainers = page.find_all('div', class_='que')
+
+questions = []
+answers = []
+for question in qContainers:
+    qText = question.find('div', class_='content').find('div', class_='formulation').find('p').text
+    questions.append(qText)
 
 print(name)
