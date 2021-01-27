8847997594d7e92fdc4af0692e683a82987ec34e
Miro Manestar <miroimanestar@gmail.com>
2021-01-26 21:27:03 -0500

    Improved moodle scraper

--- sau_moodle_scraper/moodle_scraper.py
+++ sau_moodle_scraper/moodle_scraper.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup #For parsing HTML
 from furl import furl #For... parsing HTML
 
 #Get fill-in-the-blank questions and answers
-def get_blanks(input):
+def get_multianswer(input):
     qa = [''] * 2
     for item in input.find('p').contents:
         if isinstance(item, str):
@@ -12,6 +12,9 @@ def get_blanks(input):
             qa[0] += item
         else:
             qa[0] += '____'
+
+            if item.name != 'span':
+                continue
 
             if qa[1] != '':
                 qa[1] += separator + item.find('input').attrs['value']
@@ -21,7 +24,7 @@ def get_blanks(input):
     return qa
 
 #Get questions which use a dropdown
-def get_dropdowns(input):
+def get_match(input):
     qa = [''] * 2
     qa[0] = input.find('div', class_='qtext').text
 
@@ -35,10 +38,10 @@ def get_dropdowns(input):
     return qa
 
 #Get questions which are true/false
-def get_tf(input):
+def get_truefalse(input):
     qa = [''] * 2
     qa[0] = input.find('div', class_='qtext').text
-    qa[1] = input.find('div', class_='answer').find('label').text
+    qa[1] = input.find('input', checked='checked').find_next_sibling('label').text
 
     return qa
 
@@ -51,6 +54,22 @@ def get_list(input):
             qa[1] += separator + item.find('input').attrs['value']
         else:
             qa[1] += item.find('input').attrs['value']
+
+    return qa
+
+def get_multichoice(input):
+    qa = [''] * 2
+    qa[0] = input.find('div', class_='qtext').text
+
+    for item in input.find_all('input', checked='checked'):
+        label = item.find_next_sibling('div')
+        if qa[1] != '':
+            qa[1] += separator + label.find('div').text
+        else:
+            qa[1] += label.find('div').text
+
+    return qa
+
 
 file = codecs.open('raw.html', 'r') #Get HTML file text
 
@@ -65,13 +84,16 @@ q_a = []
 
 for count, question in enumerate(qContainers):
     qItem = question.find('div', class_='content').find('div', class_='formulation') #Question item container
+    classList = question.attrs['class']
 
-    if qItem.find('p', recursive=False): #If it's a "fill-in-the-blank question"
-        q_a.append(get_blanks(qItem))
-    elif qItem.find('td'): #If it's a dropdown question
-        q_a.append(get_dropdowns(qItem))
-    elif qItem.find('input', type='radio'): #If it's a true/false question
-        q_a.append(get_tf(qItem))
+    if 'multianswer' in classList: #If it's a "fill-in-the-blank question"
+        q_a.append(get_multianswer(qItem))
+    elif 'match' in classList: #If it's a dropdown question
+        q_a.append(get_match(qItem))
+    elif 'truefalse' in classList: #If it's a true/false question
+        q_a.append(get_truefalse(qItem))
+    elif 'multichoice' in classList:
+        q_a.append(get_multichoice(qItem))
     else: #If it's a question asking for a list BROKEN
         q_a.append(qItem)
     
