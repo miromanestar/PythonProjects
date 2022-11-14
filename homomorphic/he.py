6b3fc6e78f60bffb60faa6a9fef5440c948878d3
Miro Manestar <miroimanestar@gmail.com>
2022-11-14 16:46:35 -0500

    Added he encryption example

--- homomorphic/he.py
+++ homomorphic/he.py
@@ -166,45 +166,50 @@ def mul_plain(ct, pt, q, t, poly_mod):
     new_c1 = polymul(ct[1], m, q, poly_mod)
     return (new_c0, new_c1)
 
-# Scheme's parameters
-# polynomial modulus degree
-n = 2**4
-# ciphertext modulus
-q = 2**15
-# plaintext modulus
-t = 2**8
-# polynomial modulus
-poly_mod = np.array([1] + [0] * (n - 1) + [1])
-# Keygen
-pk, sk = keygen(n, q, poly_mod)
-# Encryption
-pt1, pt2 = 73, 20
-cst1, cst2 = 7, 5
+while True:
+    # Scheme's parameters
+    # polynomial modulus degree
+    n = 2**4
+    # ciphertext modulus
+    q = 2**15
+    # plaintext modulus
+    t = 2**8
+    # polynomial modulus
+    poly_mod = np.array([1] + [0] * (n - 1) + [1])
+    # Keygen
+    pk, sk = keygen(n, q, poly_mod)
+    # Encryption
+    pt1, pt2 = 73, 20
+    cst1, cst2 = 7, 5
 
-pt1 = input("Enter the first number: ")
-pt2 = input("Enter the second number: ")
+    pt1 = int(input("Enter the first number: "))
+    pt2 = int(input("Enter the second number: "))
 
-ct1 = encrypt(pk, n, q, t, poly_mod, pt1)
-ct2 = encrypt(pk, n, q, t, poly_mod, pt2)
+    ct1 = encrypt(pk, n, q, t, poly_mod, pt1)
+    ct2 = encrypt(pk, n, q, t, poly_mod, pt2)
 
-print("[+] Ciphertext ct1({}):".format(pt1))
-print("")
-print("\t ct1_0:", ct1[0])
-print("\t ct1_1:", ct1[1])
-print("")
-print("[+] Ciphertext ct2({}):".format(pt2))
-print("")
-print("\t ct1_0:", ct2[0])
-print("\t ct1_1:", ct2[1])
-print("")
+    # print("[+] Ciphertext ct1({}):".format(pt1))
+    # print("")
+    # print("\t ct1_0:", ct1[0])
+    # print("\t ct1_1:", ct1[1])
+    # print("")
+    # print("[+] Ciphertext ct2({}):".format(pt2))
+    # print("")
+    # print("\t ct1_0:", ct2[0])
+    # print("\t ct1_1:", ct2[1])
+    # print("")
 
-# Evaluation
-ct3 = add_plain(ct1, cst1, q, t, poly_mod)
-ct4 = mul_plain(ct2, cst2, q, t, poly_mod)
+    # Evaluation
+    # ct3 = add_plain(ct1, cst1, q, t, poly_mod)
+    # ct4 = mul_plain(ct2, cst2, q, t, poly_mod)
 
-# Decryption
-decrypted_ct3 = decrypt(sk, n, q, t, poly_mod, ct3)
-decrypted_ct4 = decrypt(sk, n, q, t, poly_mod, ct4)
+    ct3 = add_plain(ct1, pt2, q, t, poly_mod)
+    ct4 = mul_plain(ct2, pt1, q, t, poly_mod)
 
-print("[+] Decrypted ct3(ct1 + {}): {}".format(cst1, decrypted_ct3))
-print("[+] Decrypted ct4(ct2 * {}): {}".format(cst2, decrypted_ct4))
+    # Decryption
+    decrypted_ct3 = decrypt(sk, n, q, t, poly_mod, ct3)
+    decrypted_ct4 = decrypt(sk, n, q, t, poly_mod, ct4)
+
+    print("[+] Decrypted ct3({} + {}): {}".format(pt1, pt2, decrypted_ct3))
+    print("[+] Decrypted ct4({} * {}): {}".format(pt1, pt2, decrypted_ct4))
+    print("")
